5efd613f2f7594139d308c5c473aff48bf0f87e4
Wiesław Pawłowski <w.pawlowski@inf.ug.edu.pl>
2014-11-14 00:29:29 +0100

    Szkielet prostej aplikacji SBT

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "SbtStarter"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+scalacOptions += "-deprecation"
+
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.4.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+object MainProg {
+
+	import com.github.nscala_time.time.Imports._
+	import org.joda.time.Days
+	import java.util.Locale
+
+	def main(args: Array[String]): Unit = {
+		val locale = new Locale("pl","pl");
+		val teraz = DateTime.now
+		val wigilia = (new DateTime)
+			.withYear(2014)
+            .withMonthOfYear(12)
+            .withDayOfMonth(24)
+
+		val doWigilii = Days.daysBetween(teraz, wigilia).getDays
+
+		println(s"Do Wigilii pozostało $doWigilii dni")
+
+		// bonus: jaki dzień będzie za 200 dni?
+		val terazPlus200 = teraz + 100.days
+		println(s"za 100 dni będzie ${terazPlus200.dayOfWeek().getAsText(locale)}")
+
+	}
+}
